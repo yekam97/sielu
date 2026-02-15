d6f688bbb3194cfc57de0c6b7f73542ff875d4b2
Camilo <yeisongamba97@gmail.com>
2026-02-15 11:41:58 -0500

    Add vite config for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                view: resolve(__dirname, 'view.html'),
+                admin: resolve(__dirname, 'admin.html'),
+            },
+        },
+    },
+});
